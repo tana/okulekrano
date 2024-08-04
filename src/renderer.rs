e1194867b4ac65ccf4577269c51dff5da6869519
Satoshi Tanaka <overthere555@gmail.com>
2024-08-05 08:42:34 +0900

    Fix incorrect color of copied image

--- src/renderer.rs
+++ src/renderer.rs
@@ -218,7 +218,7 @@ impl<'a> Renderer<'a> {
         let surface_format = surface_caps
             .formats
             .iter()
-            .find(|f| f.is_srgb())
+            .find(|f| !f.is_srgb()) // Use non-sRGB format because captured frames are already gamma-compensated
             .unwrap()
             .clone();
         self.surface.as_ref().unwrap().configure(
